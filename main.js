1fcbd056322a1e47259d2d6dca37e267a7a1ed4b
unknown <m3d13l@gmail.com>
2021-03-28 00:54:15 +0100

    Updates

--- main.js
+++ main.js
@@ -29,3 +29,19 @@ window.addEventListener('scroll', function () {
 
 });
 */
+
+// map
+
+
+function initMap() {
+
+    const loc = { lat: 31.629473, lng: -7.981084 };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: loc,
+        zoom: 12,
+    });
+
+    const marker = new google.maps.Marker({ position: loc, map: map });
+
+}
